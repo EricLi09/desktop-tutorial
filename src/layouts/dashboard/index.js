5146f765a29f522b3f6c99a57a06546fea0d31e5
EricLi09 <126361008+EricLi09@users.noreply.github.com>
2023-03-02 18:50:55 +1300

    3月2号

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class Dashboard extends Component {
     render() {
          return (
+          <Fragment>
               <div><input /><button>提交</button></div>
+              <ul>
+              <li>学英语</li>
+              <li>Learning React</li>
+              </ul>
+              </Fragment>
          )
     }
 }
